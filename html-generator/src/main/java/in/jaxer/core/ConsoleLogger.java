1d7e609718a718dd86b82973730bcdf31b08c5ac
Shakir Ansari <shakir.ansari@live.com>
2022-08-01 13:02:49 +0530

    Create ConsoleLogger.java

---
+++ html-generator/src/main/java/in/jaxer/core/ConsoleLogger.java
@@ -0,0 +1,84 @@
+package in.jaxer.core;
+
+import in.jaxer.utils.AppPropreties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Shakir
+ * @date 01-08-2022
+ * @since v1.0.0
+ */
+@Component
+public class ConsoleLogger
+{
+	@Autowired
+	private AppPropreties appPropreties;
+
+	private void log(String msg)
+	{
+		System.out.println(msg);
+	}
+
+	private void log(ConsoleLoggerType type, String msg)
+	{
+		log("[" + type.name() + "] - " + msg);
+	}
+
+	public void ln()
+	{
+		log("");
+	}
+
+	public void debug(String msg)
+	{
+		if (ConsoleLoggerType.debug.value >= appPropreties.getLoggerType().value)
+		{
+			log(ConsoleLoggerType.debug, msg);
+		}
+	}
+
+	public void info(String msg)
+	{
+		if (ConsoleLoggerType.info.value >= appPropreties.getLoggerType().value)
+		{
+			log(ConsoleLoggerType.info, msg);
+		}
+	}
+
+	public void warning(String msg)
+	{
+		if (ConsoleLoggerType.warning.value >= appPropreties.getLoggerType().value)
+		{
+			log(ConsoleLoggerType.warning, msg);
+		}
+	}
+
+	public void error(String msg)
+	{
+		if (ConsoleLoggerType.warning.value >= appPropreties.getLoggerType().value)
+		{
+			log(ConsoleLoggerType.error, msg);
+		}
+	}
+
+	public enum ConsoleLoggerType
+	{
+		debug(1000),
+		info(2000),
+		warning(3000),
+		error(4000);
+
+		private final int value;
+
+		ConsoleLoggerType(int value)
+		{
+			this.value = value;
+		}
+
+		public int getValue()
+		{
+			return value;
+		}
+	}
+}
