3ca1506eeb360b80dc5657b7a3205510c9d6a290
Shakir Ansari <shakir.ansari@live.com>
2022-08-01 13:02:57 +0530

    Create CustomFileFilter.java

---
+++ html-generator/src/main/java/in/jaxer/filters/CustomFileFilter.java
@@ -0,0 +1,39 @@
+package in.jaxer.filters;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.FileFilter;
+
+/**
+ * @author Shakir
+ * @date 26-07-2022
+ * @since v1.0.0
+ */
+@Component
+public class CustomFileFilter implements FileFilter
+{
+	@Autowired
+	private CheckIfDirectoryContainsIgnoreFileFilter checkIfDirectoryContainsIgnoreFileFilter;
+
+	@Override
+	public boolean accept(File root)
+	{
+		if (root.isDirectory())
+		{
+			if (root.isHidden())
+			{
+				return false;
+			}
+
+			File[] childFiles = root.listFiles(checkIfDirectoryContainsIgnoreFileFilter);
+			if (childFiles != null && childFiles.length > 0)
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
