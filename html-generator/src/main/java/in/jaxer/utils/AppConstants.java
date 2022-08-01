cfbd06c7eef987c929440a7573ad8567fce398a4
Shakir Ansari <shakir.ansari@live.com>
2022-08-01 13:03:05 +0530

    Create AppConstants.java

---
+++ html-generator/src/main/java/in/jaxer/utils/AppConstants.java
@@ -0,0 +1,12 @@
+package in.jaxer.utils;
+
+/**
+ * @author Shakir
+ * @date 2022-07-21
+ * @since v0.0.0
+ */
+public class AppConstants
+{
+	//	public static final String REGEX_VERSION = "-[v]{1}\\d\\.\\d(\\.\\d)?";
+	public static final String REGEX_VERSION = "-v{1}\\d{1,}\\.\\d{1,}(\\.\\d{1,})?";
+}
